f04f29a859aaf60955275ab344a206edbe4db972
Bryan Ramirez <brami007@fiu.edu>
2015-05-28 17:07:45 -0400

    Title screen added

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	
+
+	
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	public void OnMouseDownStart() {
+		Application.LoadLevel ("clicker");
+	}
+
+	public void OnMouseDownExit() {
+		Application.Quit ();
+	}
+
+	public void OnMouseDownTitle()
+	{
+		Application.CancelQuit ();
+		Application.LoadLevel ("title");
+	}
+
+}
